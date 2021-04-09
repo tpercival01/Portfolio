8a8f66eb5afcaf77de4d3b26689f987208eb40b4
UP940298 <up940298@myport.ac.uk>
2021-04-09 21:15:12 +0100

    Updated main and created social, projects and profile pages

--- core_files/main.js
+++ core_files/main.js
@@ -1,38 +1,39 @@
 var i = j = k = 0;
 
 var projectWord = "Projects";
-document.getElementById('divProjects').addEventListener("mouseover", function textAnimationProject(){
-    setTimeout(function() {
+function textAnimationProject() {
+    setTimeout(function () {
         if (i < projectWord.length) {
             document.getElementById("Projects").innerHTML += projectWord.charAt(i);
             i++;
             setTimeout(textAnimationProject, 500);
         }
     }, 500);
-});
+}
 
 var profileWord = "Profile";
-document.getElementById('divProfile').addEventListener("mouseover", function textAnimationProfile() {
-    setTimeout(function() {
+function textAnimationProfile() {
+    setTimeout(function () {
         if (j < profileWord.length) {
             document.getElementById("Profile").innerHTML += profileWord.charAt(j);
             j++;
             setTimeout(textAnimationProfile, 500);
         }
     }, 500);
-});
+};
+
 var socialWord = "Socials";
-document.getElementById('divSocials').addEventListener("mouseover", function textAnimationSocial(){
-    setTimeout(function() {
+function textAnimationSocial() {
+    setTimeout(function () {
         if (k < socialWord.length) {
             document.getElementById("Socials").innerHTML += socialWord.charAt(k);
             k++;
             setTimeout(textAnimationSocial, 500);
         }
     }, 500);
-});
+}
 
-$(document).mousemove(function(event) {
+$(document).mousemove(function (event) {
     var eye1 = $(".eye1");
     var eye2 = $(".eye2");
     var x = (eye1.offset().left) + (eye1.width() / 2);
@@ -40,15 +41,25 @@ $(document).mousemove(function(event) {
     var rad = Math.atan2(event.pageX - x, event.pageY - y);
     var rot = (rad * (180 / Math.PI) * - 1) + 180;
     eye1.css({
-        '-webkit-transform': 'rotate(' + rot +'deg)',
+        '-webkit-transform': 'rotate(' + rot + 'deg)',
         '-moz-transform': 'rotate(' + rot + 'deg)',
         '-ms-transform': 'rotate(' + rot + 'deg)',
         'transform': 'rotate(' + rot + 'deg)'
     });
     eye2.css({
-        '-webkit-transform': 'rotate(' + rot +'deg)',
+        '-webkit-transform': 'rotate(' + rot + 'deg)',
         '-moz-transform': 'rotate(' + rot + 'deg)',
         '-ms-transform': 'rotate(' + rot + 'deg)',
         'transform': 'rotate(' + rot + 'deg)'
     });
 });
+
+function timer() {
+
+    setTimeout(textAnimationProfile, ((Math.floor(Math.random() * 30) + 1) * 1000));
+    setTimeout(textAnimationProject, ((Math.floor(Math.random() * 30) + 1) * 1000));
+    setTimeout(textAnimationSocial, ((Math.floor(Math.random() * 30) + 1) * 1000));
+
+}
+
+setTimeout(timer, 5000);
